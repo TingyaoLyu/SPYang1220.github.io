5e113e44f08cb2a257ce1328546500bcbdbba563
DrustZ <z1m6r3@gmail.com>
2020-12-09 18:24:10 -0800

    add night mode support

---
+++ assets/js/drcustom.js
@@ -0,0 +1,50 @@
+// for dark mode
+//https://flaviocopes.com/dark-mode/
+const chk = document.getElementById('darkModeToggle');
+chk.addEventListener('change', () => {
+	console.log('local mode: ' + localStorage.getItem('mode'))
+	if (localStorage.getItem('mode') === 'dark'){
+		localStorage.setItem('mode', 'light'); 
+		document.querySelector('body').classList.remove('dark')
+		document.getElementById("darkModeToggle").checked = false;
+	}
+	else {
+		localStorage.setItem('mode', 'dark'); 
+		document.querySelector('body').classList.add('dark');
+		document.getElementById("darkModeToggle").checked = true;
+	}
+ //    document.body.classList.toggle('dark');
+	// document.getElementsByTagName('img')[0].classList.toggle('dark');
+});
+
+if(document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded',afterDOMLoaded);
+} else {
+    afterDOMLoaded();
+}
+
+function afterDOMLoaded(){
+	console.log('testteddsa2st')
+	console.log('init local mode: ' + localStorage.getItem('mode'))
+  	if (localStorage.getItem('mode') === 'dark'){
+		document.querySelector('body').classList.add('dark')
+		document.getElementById("darkModeToggle").checked = true;
+	}
+	else {
+		document.querySelector('body').classList.remove('dark');
+		document.getElementById("darkModeToggle").checked = false;
+	}
+}
+
+document.getElementById('_yPushState').addEventListener('y-push-state-load', function() {
+	console.log('testtest')
+	console.log('init local mode: ' + localStorage.getItem('mode'))
+  	if (localStorage.getItem('mode') === 'dark'){
+		document.querySelector('body').classList.add('dark')
+		document.getElementById("darkModeToggle").checked = true;
+	}
+	else {
+		document.querySelector('body').classList.remove('dark');
+		document.getElementById("darkModeToggle").checked = false;
+	}
+});
